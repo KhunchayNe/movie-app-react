1df0f1ab65d5f64435eed793dc8e11aa659175d2
siwapun.kh <work.siwapun.kh@gmail.com>
2024-09-12 14:49:32 +0700

    create movieCard and set mouseHover in poster

---
+++ src/components/navbar/Home/MovieCard.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { imagepath, movieCard } from '../../../utils/constants'
+
+interface MovieListProps {
+    movie: movieCard
+    key: number
+}
+
+function MovieCard({ movie, key }: MovieListProps) {
+    const [hover, setHover] = useState<number | null>(null)
+    return (
+        <div className="col"
+            onMouseEnter={() => setHover(movie.id)}
+            onMouseLeave={() => setHover(null)}>
+            <div className={`my-3 border-2 border-zinc-800 rounded-lg overflow-hidden ${movie.id === hover?'scale-[104%]':''} duration-200`} key={key}>
+                <div className="relative">
+                    <img src={imagepath + movie?.poster_path} alt="" />
+                    <div className="absolute bottom-0 w-full h-28 _carouselGradient"></div>
+                </div>
+                <div className="bg-[#222] p-2">
+                    <h1 className='text-[16px] font-semibold line-clamp-1'>{movie.title}</h1>
+                    <div className="text-[15px] text-zinc-300 mt-2">
+                        <h1 className=''>Rating : {String(movie?.vote_average).substring(0, 3)}</h1>
+                        <h1 className=''>Language : {movie?.original_language}</h1>
+                        <h1 className=''>Release : {movie?.release_date}</h1>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard
--- src/components/navbar/Home/MovieList.tsx
+++ src/components/navbar/Home/MovieList.tsx
@@ -1,4 +1,5 @@
 import { imagepath, movieCard } from '../../../utils/constants'
+import MovieCard from './MovieCard'
 
 interface MovieListProps {
     movies: movieCard[]
@@ -13,23 +14,7 @@ function MovieList({ movies }: MovieListProps) {
                 <div className="row row-cols-6">
                     {
                         movies.length > 0 && movies.map((movie, index) => (
-                            <div className="col">
-                                <div className="my-3 border-2 border-zinc-800 rounded-lg overflow-hidden" key={index}>
-                                    <div className="relative">
-                                        <img src={imagepath + movie?.poster_path} alt="" />
-                                        <div className="absolute bottom-0 w-full h-28 _carouselGradient"></div>
-                                    </div>
-                                    <div className="bg-[#222] p-2">
-                                        <h1 className='text-[16px] font-semibold line-clamp-1'>{movie.title}</h1>
-                                        <div className="text-[15px] text-zinc-300 mt-2">
-                                            <h1 className=''>Rating : {String(movie?.vote_average).substring(0, 3)}</h1>
-                                            <h1 className=''>Language : {movie?.original_language}</h1>
-                                            <h1 className=''>Release : {movie?.release_date}</h1>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
+                            <MovieCard movie={movie} key={index} />                            
                         ))
                     }
                 </div>
